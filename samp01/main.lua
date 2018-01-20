0ce3c36fb277382f2baacb1ac81a26eea46a0d91
Lacty <akira206@gmail.com>
2018-01-21 06:00:51 +0900

    get type of return and function

--- samp01/main.lua
+++ samp01/main.lua
@@ -13,3 +13,7 @@ end
 -- main.lua:12: syntax err near 'returnFunc'
 
 print(returnFunc()) -- ok
+
+print(type(returnFunc())) -- string
+print(type(returnFunc)) -- function
+print(type(114514)) -- number
