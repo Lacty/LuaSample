bd1049d95b4a9874c1d765668ab9b861c1ea67d3
Lacty <akira206@gmail.com>
2018-01-21 06:08:55 +0900

    整数、実数は区別されない

--- samp01/main.lua
+++ samp01/main.lua
@@ -10,10 +10,32 @@ function returnFunc()
     return "hi, im returnFunc"
 end
 -- print returnFunc()
--- main.lua:12: syntax err near 'returnFunc'
+-- syntax err near 'returnFunc'
 
 print(returnFunc()) -- ok
 
 print(type(returnFunc())) -- string
 print(type(returnFunc)) -- function
 print(type(114514)) -- number
+
+val = 100
+print(val) -- 100
+
+val = nil
+print(val) -- nil
+
+val = 12
+-- val += 33
+-- syntax err near '+'
+val = val + 33
+print(val) -- 45
+
+-- val
+-- syntax err near <eof>
+
+val = 8 ^ 3
+print(val) -- 512.0
+-- 8 * 8 * 8
+print (8*8*8) -- 512
+
+-- 整数、実数は区別されない
