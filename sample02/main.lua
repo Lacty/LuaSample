69f5244a42f285e246adf99a2f9b90f15db01d05
Lacty <akira206@gmail.com>
2018-01-21 07:36:16 +0900

    file read, write

--- sample02/main.lua
+++ sample02/main.lua
@@ -1,17 +1,31 @@
 print("sample 02")
 
-file = io.input("c:/Users/Yanai/Documents/LuaSample/sample02/test.txt", "w")
+--[[
+    r: read
+    w: write
+    a: create new file
+    r+: read write
+    w+: 上書き
+    a+: ファイルなかったら作成and w mode
+]]
+
+file = io.open("c:/Users/Yanai/Documents/LuaSample/sample02/test.txt", "r+")
+print(type(file)) -- userdata
 if file == nil then
     print("file: nil")
 else
     print("file: loaded")
 end
 
--- read
-for line in file:lines() do
-   print(line)
+io.input(file)
+print(io.read())
+print(io.read())
+
+for line in io.lines() do
+    print(line)
 end
 
 -- write
---file:write("from lua file\n")
---file:write("im mina.lua")
+io.output(file)
+io.write("end of file.")
+io.close() -- closeまでして処理が完了する
