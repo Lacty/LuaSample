e607540aa687128f481e40ba50c08c0257d707be
Lacty <akira206@gmail.com>
2018-01-21 05:57:54 +0900

    function test

--- samp01/main.lua
+++ samp01/main.lua
@@ -1,1 +1,15 @@
 print "hello lua"
+
+function testFunc()
+    print "testFunc"
+end
+testFunc()
+
+function returnFunc()
+    print "returnFunc"
+    return "hi, im returnFunc"
+end
+-- print returnFunc()
+-- main.lua:12: syntax err near 'returnFunc'
+
+print(returnFunc()) -- ok
