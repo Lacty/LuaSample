04ee8d14d00d4c802d2423f5854ccb2ce2da447e
Lacty <akira206@gmail.com>
2018-01-21 07:03:53 +0900

    update

--- sample02/main.lua
+++ sample02/main.lua
@@ -1,12 +1,17 @@
 print("sample 02")
 
-file = io.input("c:/Users/Yanai/Documents/LuaSample/sample02/test.txt", "r")
+file = io.input("c:/Users/Yanai/Documents/LuaSample/sample02/test.txt", "w")
 if file == nil then
     print("file: nil")
 else
     print("file: loaded")
 end
 
+-- read
 for line in file:lines() do
    print(line)
 end
+
+-- write
+--file:write("from lua file\n")
+--file:write("im mina.lua")
