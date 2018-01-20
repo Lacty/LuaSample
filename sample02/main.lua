de08b0911158da6cfefd1485c200822e25d13c56
Lacty <akira206@gmail.com>
2018-01-21 06:57:33 +0900

    ファイルの読み込み

--- sample02/main.lua
+++ sample02/main.lua
@@ -1,1 +1,12 @@
 print("sample 02")
+
+file = io.input("c:/Users/Yanai/Documents/LuaSample/sample02/test.txt", "r")
+if file == nil then
+    print("file: nil")
+else
+    print("file: loaded")
+end
+
+for line in file:lines() do
+   print(line)
+end
